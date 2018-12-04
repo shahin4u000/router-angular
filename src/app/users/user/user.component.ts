c9377942e341c4ec718c1dfc4ea8b71baa334316
Kazi Ataul Goni <kazi.shahinbd@gmail.com>
2018-12-04 15:07:39 +0100

    added

---
+++ src/app/users/user/user.component.ts
@@ -0,0 +1,27 @@
+import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user: {id: number, name: string};
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+ /*    console.log(this.route.params)
+    this.user = { id: this.route.params['id'],
+      name: this.route.paramMap['name'] }; */
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
+  }
+
+}
